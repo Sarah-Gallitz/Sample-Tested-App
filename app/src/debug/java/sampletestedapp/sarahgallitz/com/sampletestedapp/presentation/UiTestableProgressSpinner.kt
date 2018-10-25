bc42834fb9aa4952effbaec5eff842cdbd8e3dff
Sarah-Jane Gallitz <sgallitz@seek.com.au>
2018-10-25 20:54:09 +1100

    Add Resources needed for app

---
+++ app/src/debug/java/sampletestedapp/sarahgallitz/com/sampletestedapp/presentation/UiTestableProgressSpinner.kt
@@ -0,0 +1,22 @@
+package sampletestedapp.sarahgallitz.com.sampletestedapp.presentation
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.provider.Settings
+import android.support.v7.content.res.AppCompatResources.getDrawable
+import android.util.AttributeSet
+import android.widget.ProgressBar
+import sampletestedapp.sarahgallitz.com.sampletestedapp.R
+
+// Code taken and modified from https://gist.github.com/jsonfry/374c3eb5b8eb88d2a674100d6bfecd92
+class UiTestableProgressSpinner(context: Context, attrs: AttributeSet) : ProgressBar(context, attrs) {
+    override fun setIndeterminateDrawable(drawable: Drawable) {
+        val hideIndeterminateDrawable = if (Build.VERSION.SDK_INT >= 17) {
+            Settings.Global.getFloat(context.contentResolver, Settings.Global.ANIMATOR_DURATION_SCALE, 1f) == 0f
+        } else {
+            true
+        }
+        super.setIndeterminateDrawable(if (hideIndeterminateDrawable) getDrawable(context, R.drawable.loading_spinner_non_animated) else drawable)
+    }
+}
