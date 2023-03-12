36677c702d4789caa427181bfab601ffcf845ce6
Sarah Gallitz <sarah.gallitz.devs@gmail.com>
2023-03-12 14:30:23 +1100

    Add a MaterialTheme for compose

--- core/presentation/src/main/java/au/sgallitz/pokedex/BaseActivity.kt
+++ core/presentation/src/main/java/au/sgallitz/pokedex/BaseActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import au.sgallitz.pokedex.theme.Styles.Companion.PokedexTheme
 
 abstract class BaseActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Render()
+            PokedexTheme {
+                Render()
+            }
         }
     }
 
--- feature/home/presentation/src/main/java/au/sgallitz/pokedex/home/presentation/PokemonView.kt
+++ feature/home/presentation/src/main/java/au/sgallitz/pokedex/home/presentation/PokemonView.kt
@@ -1,5 +1,7 @@
 package au.sgallitz.pokedex.home.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -8,23 +10,28 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import au.sgallitz.pokedex.home.domain.model.PokemonHomeItem
 import au.sgallitz.pokedex.theme.Spacing
+import au.sgallitz.pokedex.theme.ThemedPreview
 import java.net.URL
 
 class PokemonView {
     companion object {
         @Composable
         fun RenderPokemonItem(item: PokemonHomeItem) {
-            Card(
-                Modifier.padding(Spacing.Medium)
-            ) {
-                Text(text = item.name)
+            Card {
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(Spacing.Medium)
+                ) {
+                    Text(text = item.name)
+                }
             }
         }
     }
 
     @Preview
     @Composable
-    private fun PreviewRenderPokemonItem() {
+    private fun PreviewRenderPokemonItem() = ThemedPreview {
         RenderPokemonItem(
             item = PokemonHomeItem(
                 1,
