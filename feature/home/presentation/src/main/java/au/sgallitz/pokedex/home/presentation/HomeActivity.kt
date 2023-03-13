983f0b13c54dad7869de8f5612e60c5048b3df43
Sarah Gallitz <sarah.gallitz.devs@gmail.com>
2023-03-13 22:47:53 +1100

    Fix the padding on the home grid

--- feature/home/presentation/src/main/java/au/sgallitz/pokedex/home/presentation/HomeActivity.kt
+++ feature/home/presentation/src/main/java/au/sgallitz/pokedex/home/presentation/HomeActivity.kt
@@ -1,14 +1,10 @@
 package au.sgallitz.pokedex.home.presentation
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -45,28 +40,19 @@ class HomeActivity : BaseActivity() {
         ) { paddingValues ->
             LazyVerticalGrid(
                 columns = GridCells.Adaptive(minSize = 144.dp),
-                Modifier
-                    .padding(
-                        top = paddingValues.calculateTopPadding(),
-                        start = paddingValues.calculateStartPadding(LocalLayoutDirection.current),
-                        end = paddingValues.calculateEndPadding(LocalLayoutDirection.current)
-                    )
-                    .padding(horizontal = Spacing.Medium),
                 verticalArrangement = Arrangement.spacedBy(Spacing.Medium),
-                horizontalArrangement = Arrangement.spacedBy(Spacing.Medium)
+                horizontalArrangement = Arrangement.spacedBy(Spacing.Medium),
+                contentPadding = PaddingValues(
+                    top = Spacing.Medium + paddingValues.calculateTopPadding(),
+                    bottom = Spacing.Medium + paddingValues.calculateBottomPadding(),
+                    start = Spacing.Medium + paddingValues.calculateStartPadding(LocalLayoutDirection.current),
+                    end = Spacing.Medium + paddingValues.calculateEndPadding(LocalLayoutDirection.current)
+                )
             ) {
-                item(span = { GridItemSpan(this.maxLineSpan) }) {
-                    /* adds top padding */
-                }
-
                 items(homeItems.value) { item: HomeItem ->
                     when (item) {
                         is PokemonHomeItem -> PokemonView.RenderPokemonItem(item)
                     }
-                }
-
-                item(span = { GridItemSpan(this.maxLineSpan) }) {
-                    Spacer(Modifier.fillMaxWidth().height(paddingValues.calculateBottomPadding()))
                 }
             }
         }
